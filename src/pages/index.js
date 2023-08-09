1f578c89e74bd3c296223d7bfffc98bfa5389dae
VenziUno <pendicai3@gmail.com>
2023-08-09 13:55:40 +0700

    metta

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,12 +6,36 @@ import HeroSection from "./heroSection";
 import SekilasInfo from "./sekilasInfo";
 import VisiDanMisi from "./visidanmisi";
 import Struktur from "./strucktur";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="1IoMsnjXW83fFpZ0YlMFU9EBukvRQvLCXK0nFuXxlrI"
+        />
+        <title>SDS Kristen Tabqha - Sekolah Dasar Kristen Tabqha</title>
+        <meta
+          name="description"
+          content="Selamat datang di SDS Kristen Tabqha, sekolah dasar Kristen berkualitas yang memberikan pendidikan unggul dengan pendekatan holistik."
+        />
+        <meta
+          property="og:title"
+          content="SDS Kristen Tabqha - Sekolah Dasar Kristen Tabqha"
+        />
+        <meta
+          property="og:description"
+          content="Selamat datang di SDS Kristen Tabqha, sekolah dasar Kristen berkualitas yang memberikan pendidikan unggul dengan pendekatan holistik."
+        />
+        <meta property="og:image" content="/path/to/og-image.jpg" />
+        <meta property="og:url" content="https://www.sdskristentabqha.sch.id" />
+        <link rel="icon" href="/logo.png" />
+        <link rel="canonical" href="https://www.sdskristentabqha.sch.id" />
+      </Head>
       <Layout>
         <HeroSection />
         <SekilasInfo />
